3a6beda1e968cdc9c0f09c8ea8c20d6a7bd07a0d
Sungil Saul Lee <saul@Sungils-MacBook.local>
2020-01-23 20:03:15 -0800

    Testing for project

--- src/index.js
+++ src/index.js
@@ -11,18 +11,35 @@ import { createStore } from 'redux';
 const increment = () => {
     return {
         type: 'INCREMENT'
-    }
-}
+    };
+};
+
 const decrement = () => {
     return {
         type: 'DECREMENT'
-    }
-}
+    };
+};
 
 //REDUCER
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+    }
+};
+
+let store = createStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
 
 //DISPATCH
-
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

5729992d09b87f4ef077e25ad5e25482c4f33cb6
Sungil Saul Lee <saul@Sungils-MacBook.local>
2020-01-23 20:39:02 -0800

    create actions

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const counter = useSelector(state => state.counter);
-  
+  const isLogged = useSelector(state => state.isLogged);
+
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
+      <button>+</button>
+      <button>-</button>
+      {isLogged ? <h3>Valuable information</h3> : ''}
     </div>
   );
 }

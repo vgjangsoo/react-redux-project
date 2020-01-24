9b32ce484f02afb4afe020769bc44af507d1921d
Sungil Saul Lee <saul@Sungils-MacBook.local>
2020-01-23 21:02:47 -0800

    decrement button works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment } from './actions';
+import { increment, decrement } from './actions';
 
 function App() {
   const counter = useSelector(state => state.counter);
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <h1>Counter {counter}</h1>
       <button onClick={() => dispatch(increment())}>+</button>
-      <button>-</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
       {isLogged ? <h3>Valuable information</h3> : ''}
     </div>
   );

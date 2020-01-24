84abeaf0004fee2d904f8ade5f6494760c632741
Sungil Saul Lee <saul@Sungils-MacBook.local>
2020-01-23 20:18:23 -0800

    combine reducers

--- src/index.js
+++ src/index.js
@@ -5,41 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 
-//STORE
-
-//ACTION
-const increment = () => {
-    return {
-        type: 'INCREMENT'
-    };
-};
-
-const decrement = () => {
-    return {
-        type: 'DECREMENT'
-    };
-};
-
-//REDUCER
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-    }
-};
-
-let store = createStore(counter);
-
-//Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+const store = createStore();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
